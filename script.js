a4c86deda98356488ea22dc90bd276b507762297
conster15 <41130496+conster15@users.noreply.github.com>
2025-07-20 12:33:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,167 +1,34 @@
-//<![CDATA[
-/* js/external.js */
-let get,
-  post,
-  doc,
-  htm,
-  bod,
-  nav,
-  M,
-  I,
-  mobile,
-  beacon,
-  S,
-  Q,
-  hC,
-  aC,
-  rC,
-  tC,
-  inArray,
-  shuffle,
-  isNum,
-  isInt,
-  rand; // for reuse on other loads
-addEventListener("load", () => {
-  get = (url, func, responseType = "json", context = null) => {
-    const x = new XMLHttpRequest();
-    const c = context || x;
-    x.open("GET", url);
-    x.responseType = responseType;
-    x.onload = () => {
-      if (func) func.call(c, x.response);
-    };
-    x.onerror = (e) => {
-      if (func) func.call(c, { xhrErrorEvent: e });
-    };
-    x.send();
-    return x;
-  };
-  post = (url, send, func, responseType = "json", context = null) => {
-    const x = new XMLHttpRequest();
-    if (typeof send === "object" && send && !(send instanceof Array)) {
-      const c = context || x;
-      x.open("POST", url);
-      x.responseType = responseType;
-      x.onload = () => {
-        if (func) func.call(c, x.response);
-      };
-      x.onerror = (e) => {
-        if (func) func.call(c, { xhrErrorEvent: e });
-      };
-      let d;
-      if (send instanceof FormData) {
-        d = send;
-      } else {
-        let s;
-        d = new FormData();
-        for (let k in send) {
-          s = send[k];
-          if (typeof s === "object" && s) s = JSON.stringify(s);
-          d.append(k, s);
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+
+window.onload = function(){
+ //select item
+  var puzzley = document.getElementById("gif");
+  
+  //listen to touchmove, grab location assign to gif
+  puzzley.addEventListener("touchmove", function(ev){
+    
+    var touchLocation = ev.targetTouches[0];
+    
+    puzzley.style.left = touchLocation.pageX + "px";
+    puzzley.style.top = touchLocation.pageY + "px";
+  });
+  
+puzzley.addEventListener("touchend", function(ev) {
+        //current  position when dropped
+        var x = parseInt(puzzley.style.left);
+        var y = parseInt(puzzley.style.top);
+        //check to see if that position meets our constraints
+        if (x < 388 || x > 646) {
+            puzzley.style.left = "10px";
+            puzzley.style.top = "10px";
         }
-      }
-      x.send(d);
-    } else {
-      throw new Error("send argument must be an Object");
-    }
-    return x;
-  };
-  doc = document;
-  htm = doc.documentElement;
-  bod = doc.body;
-  nav = navigator;
-  M = (tag) => doc.createElement(tag);
-  I = (id) => doc.getElementById(id);
-  mobile = nav.userAgent.match(/Mobi/i) ? true : false;
-  beacon = (url, send) => {
-    let r = false;
-    if (typeof send === "object" && send && !(send instanceof Array)) {
-      let d;
-      if (send instanceof FormData) {
-        d = send;
-      } else {
-        let s;
-        d = new FormData();
-        for (let k in send) {
-          s = send[k];
-          if (typeof s === "object" && s) s = JSON.stringify(s);
-          d.append(k, s);
+        if (y < 100 || y > 356) {
+            puzzley.style.left = "10px";
+            puzzley.style.top = "10px";
         }
-      }
-      r = nav.sendBeacon(url, d);
-    } else {
-      throw new Error("send argument must be an Object");
-    }
-    return r;
-  };
-  S = (selector, within) => {
-    var w = within || doc;
-    return w.querySelector(selector);
-  };
-  Q = (selector, within) => {
-    var w = within || doc;
-    return w.querySelectorAll(selector);
-  };
-  hC = (node, className) => {
-    return node.classList.contains(className);
-  };
-  aC = function () {
-    const a = [...arguments];
-    a.shift().classList.add(...a);
-    return aC;
-  };
-  rC = function () {
-    const a = [...arguments];
-    a.shift().classList.remove(...a);
-    return rC;
-  };
-  tC = function () {
-    const a = [...arguments];
-    a.shift().classList.toggle(...a);
-    return tC;
-  };
-  inArray = (mixed, array) => {
-    if (array.indexOf(mixed) === -1) {
-      return false;
-    }
-    return true;
-  };
-  shuffle = (array) => {
-    let a = array.slice(),
-      i = a.length,
-      n,
-      h;
-    while (i) {
-      n = Math.floor(Math.random() * i--);
-      h = a[i];
-      a[i] = a[n];
-      a[n] = h;
-    }
-    return a;
-  };
-  isNum = (mixed) => typeof mixed === "number" && !isNaN(mixed);
-  isInt = (mixed) => Number.isInteger(mixed);
-  rand = (min, max) => {
-    let mn = min,
-      mx = max;
-    if (mx === undefined) {
-      mx = mn;
-      mn = 0;
-    }
-    return mn + Math.floor(Math.random() * (mx - mn + 1));
-  };
-  // magic under here - can put on load on another page except end load
-  const test = I("test"),
-    one = I("one"),
-    two = I("two");
-  const presto = () => {
-    tC(one, "out");
-    tC(two, "out");
-  };
-  if (mobile) {
-    test.ontouchstart = test.ontouchend = presto;
-  } else {
-    test.onmouseenter = test.onmouseleave = presto;
-  }
-}); // end load
-//]]>
+    })
+}
+
+ 
